d135e0ff167bb2ec33e933aa1d7ff9e0e8df3c71
Godwill Afolabi <afolabigodwill24@gmail.com>
2023-04-23 22:50:41 -0500

    added comments to code

--- pi.go
+++ pi.go
@@ -9,8 +9,8 @@ func main() {
     n := 1000000 // number of iterations
     count := 0   // count of points inside the circle
     for i := 0; i < n; i++ {
-        x := rand.Float64()
-        y := rand.Float64()
+        x := rand.Float64()*2 - 1 // generate a random x-coordinate between -1 and 1
+        y := rand.Float64()*2 - 1 // generate a random y-coordinate between -1 and 1
         if x*x+y*y < 1 {
             count++
         }
