fd965f6c52aed1009820256cead44b44d2516f85
Godwill Afolabi <afolabigodwill24@gmail.com>
2023-04-24 23:45:51 -0500

    putting calculation in loop

--- pi.go
+++ pi.go
@@ -10,15 +10,19 @@ func main() {
 	// Define the number of iterations for each experiment
 	iterations := []int{10000, 100000, 1000000, 10000000, 100000000, 1000000000, 10000000000}
 
+	// Loop through each iteration value and perform the experiment
+    for i := 0; i < len(iterations); i++ {
+        start := time.Now()
 
-    count := 0   // count of points inside the circle
-    for i := 0; i < n; i++ {
-        x := rand.Float64()*2 - 1 // generate a random x-coordinate between -1 and 1
-        y := rand.Float64()*2 - 1 // generate a random y-coordinate between -1 and 1
-        if x*x+y*y < 1 {
-            count++
+        // Perform the Monte Carlo method to estimate pi
+        count := 0
+        for i := 0; i < n; i++ {
+            x := rand.Float64()*2 - 1
+            y := rand.Float64()*2 - 1
+            if x*x+y*y < 1 {
+                count++
+            }
         }
+        pi := 4.0 * float64(count) / float64(n)
     }
-    pi := 4.0 * float64(count) / float64(n)
-    fmt.Println("π is approximately:", pi)
 }
